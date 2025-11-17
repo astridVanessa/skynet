f8c3d8a33dfaf4698943ee9aeb7bfd14d8777d13
astrid <aecheverriar1@miumg.edu.gt>
2025-11-17 13:11:20 -0600

    fix: correct

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import app from "./app.js";
 
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, () => {
-  console.log(`🪄Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🪄 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
