58fee6e51940988704603c9e15d1d21cf4ac7f3b
astrid <aecheverriar1@miumg.edu.gt>
2025-11-17 17:34:13 -0600

    fix: correct skynet new

--- src/routes/visitaRoutes.js
+++ src/routes/visitaRoutes.js
@@ -6,15 +6,12 @@ const router = express.Router();
 
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-
 
 
 router.get("/:codigo", async (req, res) => {
