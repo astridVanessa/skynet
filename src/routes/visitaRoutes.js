d2eb7894990c7589fc9cb680b9bf626a254d7c0c
astrid <aecheverriar1@miumg.edu.gt>
2025-11-17 16:19:23 -0600

    fix: correct email server new email elastic new

--- src/routes/visitaRoutes.js
+++ src/routes/visitaRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // Elastic Email usa secure=false en puerto 2525
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
