3cfae8b44cc6c027b89249a83baf6b3cb3945ca6
astrid <aecheverriar1@miumg.edu.gt>
2025-11-17 12:26:10 -0600

    fix: run server.js + railway port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`🪄Servidor corriendo en http://localhost:${PORT}`);
