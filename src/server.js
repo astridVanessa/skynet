b9f5c727abc4f485bcf259b0bf34eee121d42c50
astrid <aecheverriar1@miumg.edu.gt>
2025-11-17 23:33:47 -0600

    fxl router smptap

--- src/server.js
+++ src/server.js
@@ -1,18 +1,9 @@
 
-import express from "express";
-import cors from "cors";
-import visitaRoutes from "./routes/visitaRoutes.js";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/visita", visitaRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API corriendo");
-});
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Servidor en puerto", PORT));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🪄 Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
+
